c951c64db0e2877cc1d2149d03acb98439bce986
Julio cesar <vcjuliocesar@outlook.com>
2020-07-17 20:32:42 -0500

    citas

---
+++ citas/src/components/Formulario.js
@@ -0,0 +1,49 @@
+import React, { Fragment } from 'react';
+
+const Formulario = () => {
+    return (
+        <Fragment>
+            <h2>Crear cita</h2>
+            <form>
+                <label>Nombre Mascota</label>
+                <input
+                    type="text"
+                    name="mascota"
+                    className="u-full-width"
+                    placeholder="Nombre Mascota"
+                />
+                <label>Nombre Dueño</label>
+                <input
+                    type="text"
+                    name="propietario"
+                    className="u-full-width"
+                    placeholder="Nombre Dueño de la Mascota"
+                />
+                <label>Fecha</label>
+                <input
+                    type="date"
+                    name="fecha"
+                    className="u-full-width"
+                />
+                <label>Hora</label>
+                <input
+                    type="time"
+                    name="hora"
+                    className="u-full-width"
+                />
+                <label>Síntomas</label>
+                <textarea
+                    className = "u-full-width"
+                    name = "sintomas"
+                >
+                </textarea>
+                <button
+                    type = "submit"
+                    className = "u-full-width button-primary"
+                >Agregar Cita</button>
+            </form>
+        </Fragment>
+    );
+}
+
+export default Formulario;
