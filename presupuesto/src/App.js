342812a18324312e298e4c9b415035bfe0944505
Julio cesar <vcjuliocesar@outlook.com>
2020-07-19 15:10:22 -0500

    Leyendo el Presupuesto del Usuario

--- presupuesto/src/App.js
+++ presupuesto/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Pregunta from './components/Pregunta';
 
 function App() {
   return (
-   <div className="container">
-     <h1>Gasto semanal</h1>
-   </div>
+    <div className="container">
+      <header>
+        <h1>Gasto semanal</h1>
+        <div className="contenido-principal contenido">
+          <Pregunta />
+        </div>
+      </header>
+    </div>
   );
 }
 
--- presupuesto/src/components/Pregunta.js
+++ presupuesto/src/components/Pregunta.js
@@ -1,15 +1,36 @@
-import React,{Fragment} from 'react';
+import React,{Fragment,useState} from 'react';
 
 const Pregunta = () => {
+    //definir estate
+    const[cantidad,guardarCantidad] = useState(0);
+
+    //funcion para leer el presupuesto
+    const definirPresupuesto = (e) => {
+        guardarCantidad(parseInt(e.target.value,10));
+    }
+
+    //submit para definir el presupuesto
+    const agregarPresupuesto = (e) =>{
+        e.eventPreventDefault();
+
+        //validar
+
+        //si pasa la validacion
+
+    }
+    
     return (
         <Fragment>
             <h2>Coloca tu presupuesto</h2>
 
-            <form>
+            <form
+                onSubmit={agregarPresupuesto}
+            >
                 <input
                     type="number"
                     className="u-full-width"
                     placeholder="Coloca tu presupiesto"
+                    onChange={definirPresupuesto}
                 />
                 <input
                     type="submit"
