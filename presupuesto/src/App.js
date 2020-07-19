c5dc5ec013ac4b7081dc18c300b15772537bd178
Julio cesar <vcjuliocesar@outlook.com>
2020-07-19 17:02:11 -0500

    Almacenando el Presupuesto

--- presupuesto/src/App.js
+++ presupuesto/src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Pregunta from './components/Pregunta';
 
 function App() {
+  //definir state
+  const [presupuesto, guardarPresupuesto] = useState(0);
+  const [restante, guardarRestante] = useState(0);
   return (
     <div className="container">
       <header>
         <h1>Gasto semanal</h1>
         <div className="contenido-principal contenido">
-          <Pregunta />
+          <Pregunta
+            guardarPresupuesto={guardarPresupuesto}
+            guardarRestante={guardarRestante}
+          />
         </div>
       </header>
     </div>
