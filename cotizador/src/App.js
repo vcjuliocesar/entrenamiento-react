2352875aff268416aa75f28413b69e1b1a11b3b2
Julio cesar <vcjuliocesar@outlook.com>
2020-07-20 11:33:08 -0500

    Creando la App y Ajustes

---
+++ cotizador/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <Header
+      titulo="Cotizador de seguros"
+    />
+  );
+}
+
+export default App;
