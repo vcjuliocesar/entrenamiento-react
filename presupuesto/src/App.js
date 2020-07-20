d598c46a3224798fb20b508de7de67a3f0bb1d9b
Julio cesar <vcjuliocesar@outlook.com>
2020-07-19 19:10:08 -0500

    Creando el Componente que mostrará el Presupuesto y el Restante

--- presupuesto/src/App.js
+++ presupuesto/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Pregunta from './components/Pregunta';
 import Formulario from './components/Formulario';
 import Listado from './components/Listado';
+import ControlPresupuesto from './components/ControlPresupuesto';
 
 function App() {
   //definir state
@@ -40,6 +41,10 @@ function App() {
                 <div className="one-half column">
                   <Listado
                     gastos={gastos}
+                  />
+                  <ControlPresupuesto
+                    presupuesto={presupuesto}
+                    restante={restante}
                   />
                 </div>
               </div>
