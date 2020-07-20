43ed5b44184f10dec145cb579683a6edd2b3afcb
Julio cesar <vcjuliocesar@outlook.com>
2020-07-20 18:49:29 -0500

    Mostrando el Total a pagar

--- cotizador/src/App.js
+++ cotizador/src/App.js
@@ -3,6 +3,7 @@ import styled from '@emotion/styled';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 import Resumen from './components/Resumen';
+import Resultado from './components/Resultado';
 
 const Contenedor = styled.div`
   max-width:600px;
@@ -25,7 +26,7 @@ function App() {
   });
 
   //extraer datos
-  const {datos} = resumen;
+  const {cotizacion,datos} = resumen;
   return (
     <Contenedor>
       <Header
@@ -37,6 +38,9 @@ function App() {
         />
         <Resumen
           datos={datos}
+        />
+        <Resultado
+          cotizacion={cotizacion}
         />
       </ContenedorFormulario>
     </Contenedor>
