fe5d32cacd9dcd2727789a70cdd2680071cd03ca
Julio cesar <vcjuliocesar@outlook.com>
2020-07-20 12:03:25 -0500

    Creando los Primeros Styled Components

--- cotizador/src/App.js
+++ cotizador/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import styled from '@emotion/styled';
 import Header from './components/Header';
+
+const Contenedor = styled.div`
+  max-width:600px;
+  margin:0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color:#FFF;
+  padding:3rem;
+`;
 
 function App() {
   return (
-    <Header
-      titulo="Cotizador de seguros"
-    />
+    <Contenedor>
+      <Header
+        titulo="Cotizador de seguros"
+      />
+      <ContenedorFormulario></ContenedorFormulario>
+    </Contenedor>
+
   );
 }
 
