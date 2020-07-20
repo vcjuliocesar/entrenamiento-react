7681801c6dd4e0f356f86d3af5bd8093a7c8a6ba
Julio cesar <vcjuliocesar@outlook.com>
2020-07-20 12:44:55 -0500

    Creando el Formulario

--- cotizador/src/App.js
+++ cotizador/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import Header from './components/Header';
+import Formulario from './components/Formulario';
 
 const Contenedor = styled.div`
   max-width:600px;
@@ -18,7 +19,9 @@ function App() {
       <Header
         titulo="Cotizador de seguros"
       />
-      <ContenedorFormulario></ContenedorFormulario>
+      <ContenedorFormulario>
+        <Formulario/>
+      </ContenedorFormulario>
     </Contenedor>
 
   );
