0c40d6a86b2d667a89efe44bede4a4f6b8abcc39
Julio cesar <vcjuliocesar@outlook.com>
2020-07-21 10:50:26 -0500

    Agregando Emotion

--- breakingbad/src/App.js
+++ breakingbad/src/App.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const Contenedor = styled.div`
+  display:flex;
+  align-items:center;
+  padding-top:5rem;
+  flex-direction:column;
+`;
+
+const Boton = styled.button`
+  background:-webkit-linear-gradient(top left, #007d35 0%,#007d35 40%,#0f574e 100%);
+  background-size:300px;
+  font-family:Arial, Helvetica, sans-serif;
+  color:#fff;
+  margin-top:3rem;
+  padding:1rem 3rem;
+  font-size:2rem;
+  border:2px solid black;
+`;
+
+const consultarAPI = () =>{
+  console.log('consultando...');
+}
 
 function App() {
   return (
-    <h1>Breking bad</h1>
+    <Contenedor>
+      <Boton
+        onClick={consultarAPI}
+      >
+        Obtener Frase
+      </Boton>
+    </Contenedor>
   );
 }
 
