476277ae192248a8733b5eb18d14319a83a3fc80
Julio cesar <vcjuliocesar@outlook.com>
2020-07-21 18:10:34 -0500

    Creando el State y Leyendo lo que el usuario selecciona

--- clima/src/components/Formulario.js
+++ clima/src/components/Formulario.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Formulario = () => {
-    return ( 
+    //state del formulario
+    const [busqueda, guardarBusqueda] = useState({
+        ciudad: '',
+        pais: ''
+    });
+
+    //extrer ciudad y pais
+    const { ciudad, pais } = busqueda;
+
+    const handleChange = (e) => {
+        //actualizar es state
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    return (
         <form>
             <div className="input-field col s12">
                 <input
                     type="text"
                     name="ciudad"
                     id="ciudad"
+                    value={ciudad}
+                    onChange={handleChange}
                 />
                 <label htmlFor="id">Ciudad:</label>
             </div>
@@ -15,13 +34,22 @@ const Formulario = () => {
                 <select
                     name="pais"
                     id="pais"
+                    value={pais}
+                    onChange={handleChange}
                 >
                     <option value="">-- Seleccione un páis --</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>
                 </select>
                 <label htmlFor="pais">Ciudad:</label>
             </div>
         </form>
-     );
+    );
 }
- 
+
 export default Formulario;
