87fe28512707f5872bd5bd0d0d3dcef86ed2ba66
Julio cesar <vcjuliocesar@outlook.com>
2020-07-22 13:03:50 -0500

    Verificando que haya resultados

--- clima/src/App.js
+++ clima/src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment, useState, useEffect } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 import Clima from './components/Clima';
+import Error from './components/Error';
 
 function App() {
   //state del formulario
@@ -12,6 +13,7 @@ function App() {
 
   const [consultar, guardarConsultar] = useState(false);
   const [resultado, guardarResultado] = useState({});
+  const [error,guardarError] = useState(false);
 
   //extrer ciudad y pais
   const { ciudad, pais } = busqueda;
@@ -25,12 +27,28 @@ function App() {
         const resultado = await respuesta.json();
         guardarResultado(resultado);
         guardarConsultar(false);
+        
+        //detectar si hay error
+        if(resultado.cod === '404'){
+          guardarError(true);
+        }else{
+          guardarError(false);
+        }
       }
 
       //http://api.openweathermap.org/data/2.5/weather?q=guadalajara,mexico&appid=1c69ab7a70f7b1d748254c0fc63354ad
     }
     consultarAPI();
   }, [consultar]);
+
+  let componente;
+  
+  //carga condicional de componentes
+  if(error){
+    componente = <Error mensaje="No hay resultados"/>
+  }else{
+    componente = <Clima resultado={resultado}/>;
+  }
 
   return (
     <Fragment>
@@ -49,9 +67,7 @@ function App() {
               />
             </div>
             <div className="col m6 s12">
-              <Clima
-                resultado={resultado}
-              />
+              {componente}
             </div>
           </div>
         </div>
--- clima/src/components/Formulario.js
+++ clima/src/components/Formulario.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Error from './Error';
 
 const Formulario = ({busqueda,guardarBusqueda,guardarConsultar}) => {
   
@@ -36,7 +37,7 @@ const Formulario = ({busqueda,guardarBusqueda,guardarConsultar}) => {
         <form
             onSubmit={handleSubmit}
         >
-            {error ? <p className="red darken-2 error">Todos los campos son obligatorios</p> : null}
+            {error ? <Error mensaje="Todos los campos son obligatorios"/> : null}
             <div className="input-field col s12">
                 <input
                     type="text"
