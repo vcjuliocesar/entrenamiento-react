ba8aad76829ea92a2de32c822efaa0bf4b898ac7
Julio cesar <vcjuliocesar@outlook.com>
2020-07-23 18:05:53 -0500

    Creando el Header y Contenedor Principal, Formulario y Módulos CSS

--- noticias/src/App.js
+++ noticias/src/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
-    <h1>Noticias</h1>
+    <Fragment>
+      <Header
+        titulo={'Buscador de Noticias'}
+      />
+
+      <div className="container white">
+        <Formulario/>
+      </div>
+    </Fragment>
   );
 }
 
