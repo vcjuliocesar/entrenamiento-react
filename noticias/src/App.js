f18e551cb7acbbf2358ad2d8bd9be251f98aad97
Julio cesar <vcjuliocesar@outlook.com>
2020-07-24 12:04:47 -0500

    Consultando la API y leyendo los resultados

--- noticias/src/App.js
+++ noticias/src/App.js
@@ -1,10 +1,27 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState,useEffect } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 
 function App() {
 
   const [categoria,guardarCategoria] = useState('');
+  const [noticias,guardarNoticias] = useState([]);
+
+  useEffect(()=>{
+    const consultarAPI = async () =>{
+      
+      const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=564ed659b3224bb29719e9dfb8019d2a`;
+      
+      const respuesta = await fetch(url);
+
+      const noticias = await respuesta.json();
+
+      guardarNoticias(noticias.articles);
+    }
+
+    consultarAPI();
+  },[categoria]);
+
   return (
     <Fragment>
       <Header
