a62c0fe770c396e512c55645d456bd80b21e9215
Julio cesar <vcjuliocesar@outlook.com>
2020-07-24 18:13:22 -0500

    Mostrando las imágenes y enlace para alta resolución

--- pixabay/src/App.js
+++ pixabay/src/App.js
@@ -13,7 +13,7 @@ function App() {
       
       if(busqueda === '') return;
 
-      const imagenesPorPagina = 10;
+      const imagenesPorPagina = 30;
       const key = '17636869-386a91d620fde7af267292499';
       const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
       
