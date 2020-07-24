d8d5bfa6e97ad7c077342d6415c389947579a27c
Julio cesar <vcjuliocesar@outlook.com>
2020-07-24 11:40:37 -0500

    Colocando la categoría en el state

--- noticias/src/App.js
+++ noticias/src/App.js
@@ -1,8 +1,10 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 
 function App() {
+
+  const [categoria,guardarCategoria] = useState('');
   return (
     <Fragment>
       <Header
@@ -10,7 +12,9 @@ function App() {
       />
 
       <div className="container white">
-        <Formulario/>
+        <Formulario 
+          guardarCategoria={guardarCategoria}
+        />
       </div>
     </Fragment>
   );
--- noticias/src/components/Formulario.js
+++ noticias/src/components/Formulario.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Formulario.module.css';
 import useSelect from '../hooks/useSelect';
 
-const Formulario = () => {
+const Formulario = ({guardarCategoria}) => {
     const OPCIONES = [
         {value:'general',label:'General'},
         {value:'business',label:'Negocios'},
@@ -12,15 +12,22 @@ const Formulario = () => {
         {value:'sports',label:'Deportes'},
         {value:'technology',label:'Tecnología'}
     ];
-    /**
-     * business entertainment general health science sports technology
-     */
+    
     //utilizar custom hook
     const[categoria,SelectCategoria] = useSelect('general',OPCIONES);
+
+    const buscarNotificas = (e) => {
+        e.preventDefault();
+
+        guardarCategoria(categoria);
+    }
+
     return (
         <div className={`${styles.buscador} row`}>
             <div className="col s12 m8 offset-m2">
-                <form>
+                <form
+                    onSubmit={buscarNotificas}
+                >
                     <h2 className={styles.heading}>Encuentra Noticias por Categoria</h2>
                     <SelectCategoria/>
                     <div className="input-field col s12">
