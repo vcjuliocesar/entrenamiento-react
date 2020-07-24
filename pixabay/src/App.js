e4eb6db885698b338a1c00e891161e0903377321
Julio cesar <vcjuliocesar@outlook.com>
2020-07-24 15:39:17 -0500

    Creando la App y Primeros Ajustes

---
+++ pixabay/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Formulario from './components/Formlario';
+
+function App() {
+  return (
+   <div className="container">
+     <div className="jumbotron">
+       <p className="lead text-center">Buscador de imágenes</p>
+       <Formulario/>
+     </div>
+   </div>
+  );
+}
+
+export default App;
