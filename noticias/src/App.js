37ea0cff78c2f176ecbf4d25c350ff825cf29cac
Julio cesar <vcjuliocesar@outlook.com>
2020-07-24 12:41:44 -0500

    Creando un Listado de Noticias

--- noticias/src/App.js
+++ noticias/src/App.js
@@ -1,17 +1,18 @@
-import React, { Fragment, useState,useEffect } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
+import ListadoNoticias from './components/ListadoNoticias';
 
 function App() {
 
-  const [categoria,guardarCategoria] = useState('');
-  const [noticias,guardarNoticias] = useState([]);
+  const [categoria, guardarCategoria] = useState('');
+  const [noticias, guardarNoticias] = useState([]);
 
-  useEffect(()=>{
-    const consultarAPI = async () =>{
-      
+  useEffect(() => {
+    const consultarAPI = async () => {
+
       const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=564ed659b3224bb29719e9dfb8019d2a`;
-      
+
       const respuesta = await fetch(url);
 
       const noticias = await respuesta.json();
@@ -20,7 +21,7 @@ function App() {
     }
 
     consultarAPI();
-  },[categoria]);
+  }, [categoria]);
 
   return (
     <Fragment>
@@ -29,8 +30,11 @@ function App() {
       />
 
       <div className="container white">
-        <Formulario 
+        <Formulario
           guardarCategoria={guardarCategoria}
+        />
+        <ListadoNoticias
+          noticias={noticias}
         />
       </div>
     </Fragment>
