86098cf4e60206b0c40a3ca85ff8a119f4ed822d
Julio cesar <vcjuliocesar@outlook.com>
2020-07-27 18:17:56 -0500

    Obteniendo la letra de la canción

--- letras/src/App.js
+++ letras/src/App.js
@@ -1,10 +1,33 @@
-import React,{Fragment} from 'react';
+import React,{Fragment,useState,useEffect} from 'react';
 import Formulario from './components/Formulario';
+import axios from 'axios';
 
 function App() {
+  
+  const[busquedaletra,guardarBusquedaLetra] = useState({});
+  const[letra,guardarLetra] = useState('');
+
+  useEffect(()=>{
+    if(Object.keys(busquedaletra).length === 0) return;
+
+    const consultarApiLetra = async () =>{
+      
+      const {artista,cancion} = busquedaletra;
+
+      const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+
+      const resultado = await axios.get(url);
+
+      guardarLetra(resultado.data.lyrics);
+    };
+    consultarApiLetra();
+  },[busquedaletra]);
+  
   return (
    <Fragment>
-     <Formulario/>
+     <Formulario
+      guardarBusquedaLetra={guardarBusquedaLetra}
+     />
    </Fragment>
   );
 }
--- letras/src/components/Formulario.js
+++ letras/src/components/Formulario.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const Formulario = () => {
+const Formulario = ({guardarBusquedaLetra}) => {
 
     const [busqueda, guardarBusqueda] = useState({
         artista: '',
@@ -26,12 +26,13 @@ const Formulario = () => {
             guardarError(true);
             return;
         }
-        
         guardarError(false);
+        guardarBusquedaLetra(busqueda);
     }
 
     return (
         <div className="bg-info">
+             {error ? <p className="alert alert-danger text-center p-2">Todos los campos son obligatorios</p>:null}
             <div className="container">
                 <div className="row">
                     <form
