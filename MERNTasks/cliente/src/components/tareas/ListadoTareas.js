3fff517a0e8fba78093f44f8b4d2bf39dd960031
Julio cesar <vcjuliocesar@outlook.com>
2020-07-30 10:24:58 -0500

    Creando el Listado de Tareas

---
+++ MERNTasks/cliente/src/components/tareas/ListadoTareas.js
@@ -0,0 +1,30 @@
+import React,{Fragment} from 'react';
+import Tarea from './Tarea';
+
+const ListadoTareas = () => {
+    const tareasProyecto=[
+        {nombre:'Elegir Plataforma',estado:true},
+        {nombre:'Elegir Colores',estado:true},
+        {nombre:'Elegir Plataformas de pago',estado:true},
+        {nombre:'Elegir Histing',estado:true}
+    ];
+    return ( 
+        <Fragment>
+            <h2>Proyecto: Tienda Virtual</h2>
+            <ul className="listado-tareas">
+                {tareasProyecto.length === 0
+                    ?(<li className="tarea"><p>No hay tareas</p></li>)
+                    
+                    :tareasProyecto.map(tarea=>(
+                        <Tarea
+                            tarea={tarea}
+                        />
+                    ))
+                }
+            </ul>
+        </Fragment>
+        
+     );
+}
+ 
+export default ListadoTareas;
