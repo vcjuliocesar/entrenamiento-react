e7bc04a60d3e14dcd05eab84d146629291d27359
Julio cesar <vcjuliocesar@outlook.com>
2020-07-31 15:41:20 -0500

    Ocultando el Formulario de Tareas si no hay Proyecto Seleccionado

--- MERNTasks/cliente/src/components/tareas/FormTarea.js
+++ MERNTasks/cliente/src/components/tareas/FormTarea.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React,{useContext} from 'react';
+import proyectoContext from '../../context/proyectos/proyectoContext';
 
 const FormTarea = () => {
+     //Extraer si un proyecto esta activo
+     const proyectosContext = useContext(proyectoContext);
+     const { proyecto } = proyectosContext;
+ 
+     //si no hay proyecto seleccionado
+     if(!proyecto) return null;
+    //Array destructuring para extraer el proyecto actual
+    const [proyectoActual] = proyecto;
     return (  
         <div className="formulario">
             <form>
