16181bc0f314de1952f7091c73752800c5c66408
Julio cesar <vcjuliocesar@outlook.com>
2020-08-03 15:44:25 -0500

    Agregando una animación a los Proyectos

--- MERNTasks/cliente/src/components/proyectos/ListadoProyectos.js
+++ MERNTasks/cliente/src/components/proyectos/ListadoProyectos.js
@@ -1,28 +1,36 @@
-import React,{useContext,useEffect} from 'react';
+import React, { useContext, useEffect } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import Proyecto from './Proyecto';
 import proyectoContext from '../../context/proyectos/proyectoContext';
 
 const ListadoProyectos = () => {
     //extraer proyectos de state inical
     const proyectosContext = useContext(proyectoContext);
-    const {proyectos,obtenerProyectos} = proyectosContext;
+    const { proyectos, obtenerProyectos } = proyectosContext;
     //obtener proectos cuando carga el componente
-    useEffect(()=>{
+    useEffect(() => {
         obtenerProyectos();
-    },[]);
+    }, []);
     //validar si hay proyectos
-    if(proyectos.length === 0) return <p>No hay proyectos</p>;
+    if (proyectos.length === 0) return <p>No hay proyectos</p>;
 
-    return (  
+    return (
         <ul className="listado-proyectos">
-            {proyectos.map(proyecto => (
-                <Proyecto
-                    key={proyecto.id}
-                    proyecto={proyecto}
-                />
-            ))}
+            <TransitionGroup>
+                {proyectos.map(proyecto => (
+                    <CSSTransition
+                        key={proyecto.id}
+                        timeout={200}
+                        classNames="proyecto"
+                    >
+                        <Proyecto
+                            proyecto={proyecto}
+                        />
+                    </CSSTransition>
+                ))}
+            </TransitionGroup>
         </ul>
     );
 }
- 
+
 export default ListadoProyectos;
