c993e098412c47af508f5b36e6d2e146b44d3ee9
Julio cesar <vcjuliocesar@outlook.com>
2020-08-03 15:28:43 -0500

    Agregando una animación cuando se crean nuevas tareas

--- MERNTasks/cliente/src/components/tareas/ListadoTareas.js
+++ MERNTasks/cliente/src/components/tareas/ListadoTareas.js
@@ -1,40 +1,50 @@
-import React,{Fragment,useContext} from 'react';
+import React, { Fragment, useContext } from 'react';
 import proyectoContext from '../../context/proyectos/proyectoContext';
 import tareaContext from '../../context/tareas/tareaContext';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import Tarea from './Tarea';
 
 const ListadoTareas = () => {
-    
+
     //obtener el state del 
     const proyectosContext = useContext(proyectoContext);
-    const { proyecto,eliminarProyecto } = proyectosContext;
+    const { proyecto, eliminarProyecto } = proyectosContext;
 
     //obtener las ateras del proyecto
     const tareasContext = useContext(tareaContext);
     const { tareasproyecto } = tareasContext;
 
     //si no hay proyecto seleccionado
-    if(!proyecto) return <h2>Seleciona un proyecto</h2>
+    if (!proyecto) return <h2>Seleciona un proyecto</h2>
     //Array destructuring para extraer el proyecto actual
     const [proyectoActual] = proyecto;
 
     //eliminar proyecto
-    const onClickEliminar = ()=>{
+    const onClickEliminar = () => {
         eliminarProyecto(proyectoActual.id)
     }
-    return ( 
+    return (
         <Fragment>
             <h2>Proyecto: {proyectoActual.nombre}</h2>
             <ul className="listado-tareas">
                 {tareasproyecto.length === 0
-                    ?(<li className="tarea"><p>No hay tareas</p></li>)
-                    
-                    :tareasproyecto.map(tarea=>(
-                        <Tarea
-                            key={tarea.id}
-                            tarea={tarea}
-                        />
-                    ))
+                    ? (<li className="tarea"><p>No hay tareas</p></li>)
+
+                    :
+                    <TransitionGroup>
+                        {tareasproyecto.map(tarea => (
+                            <CSSTransition
+                                key={tarea.id}
+                                timeout={200}
+                                classNames="tarea"
+                            >
+                                <Tarea
+                                    tarea={tarea}
+                                />
+                            </CSSTransition>
+                        ))}
+                    </TransitionGroup>
+
                 }
             </ul>
             <button
@@ -43,8 +53,8 @@ const ListadoTareas = () => {
                 onClick={onClickEliminar}
             >Eliminar proyecto &times;</button>
         </Fragment>
-        
-     );
+
+    );
 }
- 
+
 export default ListadoTareas;
