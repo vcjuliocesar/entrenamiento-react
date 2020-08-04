07130b31648cb64d40b0c8686b818f3ce19aab04
Julio cesar <vcjuliocesar@outlook.com>
2020-08-04 12:39:16 -0500

    Creando el Servidor de Express

--- MERNTasks/servidor/index.js
+++ MERNTasks/servidor/index.js
@@ -1,1 +1,13 @@
-console.log("Desde index js");
+const express = require('express');
+
+//crear el servidor
+const app = express();
+
+//puerto de la app
+const PORT = process.env.PORT || 4000;
+
+//arrancar la app
+app.listen(PORT,()=>{
+    console.log(`El servidor esta corriendo en el puerto ${PORT}`)
+})
+
