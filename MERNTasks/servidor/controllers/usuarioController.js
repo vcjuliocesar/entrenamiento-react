fbbf5d3e8458a40b44d67ba54b2712fdea72e333
Julio cesar <vcjuliocesar@outlook.com>
2020-08-04 17:20:46 -0500

    Insertando usuarios en la BD

--- MERNTasks/servidor/controllers/usuarioController.js
+++ MERNTasks/servidor/controllers/usuarioController.js
@@ -1,3 +1,15 @@
-exports.crearUsuario = (req,resp) => {
-    console.log(req.body);
+const Usuario = require("../models/Usuario");
+
+exports.crearUsuario = async (req,res) => {
+    try {
+        //crear usuario
+        let usuario = new Usuario(req.body);
+        //guardar usuario
+        await usuario.save();
+        //mensaje
+        res.send('Usuario Registrado correctamente');
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('Hubo un error');
+    }
 }
