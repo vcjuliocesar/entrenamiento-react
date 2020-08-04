a49a1cccff346ad69976b4a99bf8bc62c03679d3
Julio cesar <vcjuliocesar@outlook.com>
2020-08-04 18:13:10 -0500

    Agregando validación a la API

--- MERNTasks/servidor/controllers/usuarioController.js
+++ MERNTasks/servidor/controllers/usuarioController.js
@@ -1,14 +1,21 @@
 const Usuario = require("../models/Usuario");
 const bcryptjs = require("bcryptjs");
+const { validationResult } = require("express-validator");
 
-exports.crearUsuario = async (req,res) => {
+exports.crearUsuario = async (req, res) => {
+    //validar si hay errores
+    const errores = validationResult(req);
+    
+    if(!errores.isEmpty()){
+        return res.status(400).json({errores:errores.array()});
+    }
     //extraer email y password
-    const {email,password} = req.body;
+    const { email, password } = req.body;
     try {
-        let usuario = await Usuario.findOne({email});
+        let usuario = await Usuario.findOne({ email });
 
-        if(usuario){
-            return res.status(400).json({msg:"El usuario ya existe"});
+        if (usuario) {
+            return res.status(400).json({ msg: "El usuario ya existe" });
         }
 
         //crear usuario
@@ -16,14 +23,14 @@ exports.crearUsuario = async (req,res) => {
 
         //hashear password
         const salt = await bcryptjs.genSalt(10);
-        usuario.password = await bcryptjs.hash(password,salt);
-        
+        usuario.password = await bcryptjs.hash(password, salt);
+
         //guardar usuario
         await usuario.save();
         //mensaje
-        res.json({msg:'Usuario Registrado correctamente'});
+        res.json({ msg: 'Usuario Registrado correctamente' });
     } catch (error) {
         console.log(error);
-        res.status(400).json({msg:'Hubo un error'});
+        res.status(400).json({ msg: 'Hubo un error' });
     }
 }
