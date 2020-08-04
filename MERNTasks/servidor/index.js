093177e095b8d8508c598a0780d0fa5914c78979
Julio cesar <vcjuliocesar@outlook.com>
2020-08-04 13:12:38 -0500

    Conectando MongoDB a nuestra aplicación

--- MERNTasks/servidor/index.js
+++ MERNTasks/servidor/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const conectarDB = require('./config/db');
 
 //crear el servidor
 const app = express();
+
+//conectarse a la bd
+conectarDB();
 
 //puerto de la app
 const PORT = process.env.PORT || 4000;
