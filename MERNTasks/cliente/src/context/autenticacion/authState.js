602faede7a8d22e967f73fa9145ef69349c1a159
Julio cesar <vcjuliocesar@outlook.com>
2020-08-16 18:06:44 -0500

    Creando el State y Provider

---
+++ MERNTasks/cliente/src/context/autenticacion/authState.js
@@ -0,0 +1,38 @@
+import React, { useReducer } from 'react';
+import AuthContext from './authContext';
+import AuthReducer from './authReducer';
+import {
+    REGISTRO_EXITOSO,
+    REGISTRO_ERROR,
+    OBTENER_USUARIO,
+    LOGIN_EXITOSO,
+    LOGIN_ERROR,
+    CERRAR_SESSION
+} from '../../types';
+
+
+const AuthState = (props) => {
+    const initialState = {
+        token: localStorage.getItem('token'),
+        autenticado: null,
+        usuario: null,
+        mensaje: null
+    }
+
+    const [state, dispatch] = useReducer(AuthReducer, initialState);
+    //funciones
+    return (
+        <AuthContext.Provider
+            value={{
+                token: state.token,
+                autenticado: state.autenticado,
+                usuario: state.usuario,
+                mensaje: state.mensaje
+            }}
+        >
+            {props.children}
+        </AuthContext.Provider>
+    );
+}
+
+export default AuthState;
